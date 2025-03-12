16846c221e4580720b547f2090235e9831209e0a
TomWall1 <thomasjameswall1@gmail.com>
2025-03-12 21:41:06 +1100

    Wrap App with XeroProvider for Xero OAuth integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,26 @@ import NavHeader from './components/NavHeader';
 import Home from './pages/Home';
 import Upload from './pages/Upload';
 import Results from './pages/Results';
-import AccountLinks from './pages/AccountLinks'; // New page for account linking
+import AccountLinks from './pages/AccountLinks';
 import XeroCallback from './components/XeroCallback';
+import { XeroProvider } from './context/XeroContext';
 
 function App() {
   return (
-    <div className="App">
-      <NavHeader />
-      <div className="container mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/account-links" element={<AccountLinks />} />
-          <Route path="/auth/xero/callback" element={<XeroCallback />} />
-        </Routes>
+    <XeroProvider>
+      <div className="App">
+        <NavHeader />
+        <div className="container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/account-links" element={<AccountLinks />} />
+            <Route path="/auth/xero/callback" element={<XeroCallback />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </XeroProvider>
   );
 }
 
