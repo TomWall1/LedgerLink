f6045778173f6e39b77c9190b98d492d3fb69e46
TomWall1 <thomasjameswall1@gmail.com>
2025-03-20 23:01:43 +1100

    Update App.js to fix container issues with new landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,17 +12,31 @@ import { XeroProvider } from './context/XeroContext';
 function App() {
   return (
     <XeroProvider>
-      <div className="App bg-gray-50"> {/* Changed from just App to add bg-gray-50 class */}
+      <div className="App bg-white"> {/* Changed from bg-gray-50 to bg-white */}
         <NavHeader />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/upload" element={<Upload />} />
-            <Route path="/results" element={<Results />} />
-            <Route path="/account-links" element={<AccountLinks />} />
-            <Route path="/auth/xero/callback" element={<XeroCallback />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/upload" element={
+            <div className="container">
+              <Upload />
+            </div>
+          } />
+          <Route path="/results" element={
+            <div className="container">
+              <Results />
+            </div>
+          } />
+          <Route path="/account-links" element={
+            <div className="container">
+              <AccountLinks />
+            </div>
+          } />
+          <Route path="/auth/xero/callback" element={
+            <div className="container">
+              <XeroCallback />
+            </div>
+          } />
+        </Routes>
       </div>
     </XeroProvider>
   );
