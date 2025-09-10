d922c7f53b11b35130cf09c4387ce546030c0eba
TomWall1 <thomasjameswall1@gmail.com>
2025-09-10 23:12:19 +1000

    Update main App component to use new LedgerLink dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,11 @@
-/**
- * Main LedgerLink Application
- * 
- * This is your main application component that brings together
- * all the data source integrations (CSV, Xero, Coupa)
- */
-
 import React from 'react';
-import DataSourceDashboard from './components/DataSourceDashboard';
-import CSVUpload from './components/CSVUpload';
-import XeroIntegration from './components/XeroIntegration';
-import CoupaConnection from './components/CoupaConnection';
-import CoupaDataPreview from './components/CoupaDataPreview';
-import DataPreview from './components/DataPreview';
-import ReconciliationDashboard from './components/ReconciliationDashboard';
+import LedgerLinkDashboard from './components/LedgerLinkDashboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <DataSourceDashboard />
+      <LedgerLinkDashboard />
     </div>
   );
 }
