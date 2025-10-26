8ab5795db69176f9e3f95bac7f151fd2e59f82d1
TomWall1 <thomasjameswall1@gmail.com>
2025-10-26 22:17:34 +1100

    Fix: Convert User.js to ES6 module syntax for Render deployment

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -71,4 +71,4 @@ userSchema.methods.toJSON = function() {
   return user;
 };
 
-module.exports = mongoose.model('User', userSchema);
+export default mongoose.model('User', userSchema);
